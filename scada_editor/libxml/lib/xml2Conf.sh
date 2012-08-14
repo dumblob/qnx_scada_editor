13e713bc8bf3fccd6f2028c9976ee7f08993ec55
xpacne00 <xpacne00@bda6da93-004a-4ae9-8e07-715c10848801>
2012-08-14 17:25:35 +0000

    xpacne00/ branch cleanup
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/SCADA/branches/xpacne00@50 bda6da93-004a-4ae9-8e07-715c10848801

--- scada_editor/libxml/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/root/libxml/lib"
-XML2_LIBS="-lxml2 -lz   -lm -lsocket "
-XML2_INCLUDEDIR="-I/root/libxml/include/libxml2"
-MODULE_VERSION="xml2-2.8.0"
-
--- scada_editor/src/open_file.h
+++
@@ -1,6 +0,0 @@
-#ifndef OPEN_FILE_H
-#define OPEN_FILE_H
-
-int showFileSelector(PtFileSelectionInfo_t *, const char *);
-
-#endif
